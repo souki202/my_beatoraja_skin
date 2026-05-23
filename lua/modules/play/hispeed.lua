894fec9e44aae5403436930b07a3b55ead45beaa
Totori <290Livermorium@gmail.com>
2026-05-23 19:05:39 +0900

    数字

--- lua/modules/play/hispeed.lua
+++ lua/modules/play/hispeed.lua
@@ -44,11 +44,23 @@ local HISPEED = {
             X_W = function (self) return lanes.getAreaX() + lanes.getAreaW() * 1 / 4 - self.NUM.W * self.NUM.DIGIT / 2 end,
             X_B = function (self) return lanes.getAreaX() + lanes.getAreaW() * 2 / 4 - self.NUM.W * self.NUM.DIGIT / 2 end,
             X_G = function (self) return lanes.getAreaX() + lanes.getAreaW() * 3 / 4 - self.NUM.W * self.NUM.DIGIT / 2 end,
+            X_B_SMALL = function (self) return lanes.getAreaX() + lanes.getAreaW() * 2 / 4 - self.NUM.COVER.SMALL.W * self.NUM.COVER.SMALL.DIGIT / 2 end,
+            X_G_SMALL = function (self) return lanes.getAreaX() + lanes.getAreaW() * 3 / 4 - self.NUM.COVER.SMALL.W * self.NUM.COVER.SMALL.DIGIT / 2 end,
             LANE = {
                 Y = function () return HEIGHT + 10 end,
             },
             LIFT = {
                 Y = function () return lanes.getAreaNormalY() - 32 end
+            },
+            NO_LIFT = {
+                Y = function () return lanes.getAreaNormalY() + 70 end
+            },
+            SMALL = {
+                DIGIT = 4,
+                W = 10,
+                H = 12,
+                GAP = 2,
+                RANGE_GAP = 3,
             },
             W = 17,
             H = 22,
@@ -176,9 +188,13 @@ hispeed.functions.load = function ()
                 if ref == 1319 then ref = 312 end
                 if ref == 1316 then ref = 1312 end
                 if ref == 1318 then ref = 1314 end
+                local align = 2
+                if HISPEED.IDS.TYPE[i] == "max" then
+                    align = 0
+                end
                 vals[#vals+1] = {
                     id = HISPEED.IDS.TYPE[i] .. HISPEED.IDS.COLOR[j] .. HISPEED.IDS.COVER[k],
-                    src = 0, x = 1928, y = srcY, w = GREEN.NUM.W * 10, h = GREEN.NUM.H, divx = 10, digit = GREEN.NUM.DIGIT, align = 2, space = -2,
+                    src = 0, x = 1928, y = srcY, w = GREEN.NUM.W * 10, h = GREEN.NUM.H, divx = 10, digit = GREEN.NUM.DIGIT, align = align, space = -2,
                     ref = ref
                 }
             end
@@ -245,47 +261,94 @@ hispeed.functions.dst = function ()
         -- レーンカバー部分
         local nw = HISPEED.NUM.W
         local nh = HISPEED.NUM.H
+        local small = HISPEED.NUM.COVER.SMALL
+        local function addCoverNumber(id, op, offset, x, y, w, h)
+            local item = {
+                id = id, op = op, dst = {
+                    {x = x, y = y, w = w, h = h}
+                }
+            }
+            if offset ~= nil then
+                item.offset = offset
+            end
+            dst[#dst+1] = item
+        end
+        local function addSmallRange(color, cover, op, offset, x, mainY, position)
+            local rangeY = mainY + nh + small.GAP
+            if position == "below" then
+                rangeY = mainY - small.H - small.GAP
+            end
+            local centerX = x + small.W * small.DIGIT / 2
+            local hyphenX = centerX - HISPEED.HYPHEN.W / 2
+            local minX = hyphenX - small.RANGE_GAP - small.W * small.DIGIT
+            local maxX = hyphenX + HISPEED.HYPHEN.W + small.RANGE_GAP
+            local hyphenId = color == "Green" and "greenHyphen" or "blueHyphen"
+            addCoverNumber("min" .. color .. cover, op, offset, minX, rangeY, small.W, small.H)
+            addCoverNumber(hyphenId, op, offset, hyphenX, rangeY, HISPEED.HYPHEN.W, HISPEED.HYPHEN.H)
+            addCoverNumber("max" .. color .. cover, op, offset, maxX, rangeY, small.W, small.H)
+        end
+        local laneY = HISPEED.NUM.COVER.LANE.Y()
+        local liftY = HISPEED.NUM.COVER.LIFT.Y()
+        local noLiftY = HISPEED.NUM.COVER.NO_LIFT.Y()
         dst[#dst+1] = {
             id = "laneCoverValue", op = {80, {270}, {271}}, offset = 4, dst = {
-                {x = HISPEED.NUM.COVER.X_W(HISPEED), y = HISPEED.NUM.COVER.LANE.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_W(HISPEED), y = laneY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeGreenValueCoverOn", op = {80, {270}, {271}}, offset = 4, dst = {
-                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = HISPEED.NUM.COVER.LANE.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = laneY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeBlueValueCoverOn", op = {80, {270}, {271}}, offset = 4, dst = {
-                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = HISPEED.NUM.COVER.LANE.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = laneY, w = nw, h = nh}
             }
         }
+        addSmallRange("Green", "On", {80, {270}, {271}}, 4, HISPEED.NUM.COVER.X_G_SMALL(HISPEED), laneY, "above")
+        addSmallRange("Blue", "On", {80, {270}, {271}}, 4, HISPEED.NUM.COVER.X_B_SMALL(HISPEED), laneY, "above")
+
         -- リフト部分
         dst[#dst+1] = {
             id = "liftValue", op = {80, {270}, {272}}, offset = 3, dst = {
-                {x = HISPEED.NUM.COVER.X_W(HISPEED), y = HISPEED.NUM.COVER.LIFT.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_W(HISPEED), y = liftY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeGreenValueCoverOn", op = {80, 271, {270}, {272}}, offset = 3, dst = {
-                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = HISPEED.NUM.COVER.LIFT.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = liftY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeGreenValueCoverOff", op = {80, -271, {270}, {272}}, offset = 3, dst = {
-                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = HISPEED.NUM.COVER.LIFT.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_G(HISPEED), y = liftY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeBlueValueCoverOn", op = {80, 271, {270}, {272}}, offset = 3, dst = {
-                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = HISPEED.NUM.COVER.LIFT.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = liftY, w = nw, h = nh}
             }
         }
         dst[#dst+1] = {
             id = "largeBlueValueCoverOff", op = {80, -271, {270}, {272}}, offset = 3, dst = {
-                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = HISPEED.NUM.COVER.LIFT.Y(), w = nw, h = nh}
+                {x = HISPEED.NUM.COVER.X_B(HISPEED), y = liftY, w = nw, h = nh}
             }
         }
+        addSmallRange("Green", "On", {80, 271, {270}, {272}}, 3, HISPEED.NUM.COVER.X_G_SMALL(HISPEED), liftY, "below")
+        addSmallRange("Green", "Off", {80, -271, {270}, {272}}, 3, HISPEED.NUM.COVER.X_G_SMALL(HISPEED), liftY, "below")
+        addSmallRange("Blue", "On", {80, 271, {270}, {272}}, 3, HISPEED.NUM.COVER.X_B_SMALL(HISPEED), liftY, "below")
+        addSmallRange("Blue", "Off", {80, -271, {270}, {272}}, 3, HISPEED.NUM.COVER.X_B_SMALL(HISPEED), liftY, "below")
+
+        -- リフトがないとき
+        addCoverNumber("laneCoverValue", {80, {270}, -272}, nil, HISPEED.NUM.COVER.X_W(HISPEED), noLiftY, nw, nh)
+        addCoverNumber("largeGreenValueCoverOn", {80, 271, {270}, -272}, nil, HISPEED.NUM.COVER.X_G(HISPEED), noLiftY, nw, nh)
+        addCoverNumber("largeGreenValueCoverOff", {80, -271, {270}, -272}, nil, HISPEED.NUM.COVER.X_G(HISPEED), noLiftY, nw, nh)
+        addCoverNumber("largeBlueValueCoverOn", {80, 271, {270}, -272}, nil, HISPEED.NUM.COVER.X_B(HISPEED), noLiftY, nw, nh)
+        addCoverNumber("largeBlueValueCoverOff", {80, -271, {270}, -272}, nil, HISPEED.NUM.COVER.X_B(HISPEED), noLiftY, nw, nh)
+        addSmallRange("Green", "On", {80, 271, {270}, -272}, nil, HISPEED.NUM.COVER.X_G_SMALL(HISPEED), noLiftY, "below")
+        addSmallRange("Green", "Off", {80, -271, {270}, -272}, nil, HISPEED.NUM.COVER.X_G_SMALL(HISPEED), noLiftY, "below")
+        addSmallRange("Blue", "On", {80, 271, {270}, -272}, nil, HISPEED.NUM.COVER.X_B_SMALL(HISPEED), noLiftY, "below")
+        addSmallRange("Blue", "Off", {80, -271, {270}, -272}, nil, HISPEED.NUM.COVER.X_B_SMALL(HISPEED), noLiftY, "below")
     end
 
     return skin
