a06b8d763a969e31db14a6e68a1fa97aa70d362b
Totori <290Livermorium@gmail.com>
2026-05-29 17:53:08 +0900

    update

--- lua/modules/result/user_info.lua
+++ lua/modules/result/user_info.lua
@@ -6,6 +6,8 @@ local userInfo = {
     functions = {}
 }
 
+local CLIENT_VERSION_REF = 1010
+
 local USER_INFO = {
     SIZE = 16,
     WND = {
@@ -14,25 +16,48 @@ local USER_INFO = {
         W = WND_WIDTH,
         H = 44,
     },
-    DATE = {
+    PLAYER = {
         X = function (self) return self.WND.X + 18 end,
+        Y = function (self) return self.WND.Y + 20 end,
+        W = 200,
+    },
+    CLIENT = {
+        X = function (self) return self.WND.X + 325 end,
+        Y = function (self) return self.WND.Y + 20 end,
+        W = 250,
+    },
+    DATE = {
+        X = function (self) return self.WND.X + self.WND.W - 18 end,
         Y = function (self) return self.WND.Y + 20 end,
         W = 160,
     },
-    NAME = {
-        X = function (self) return self.WND.X + self.WND.W - 18 end,
-        Y = function (self) return self.WND.Y + 20 end,
-        W = 460,
-    }
 }
 
 userInfo.functions.change2p = function ()
     USER_INFO.WND.X = LEFT_X
 end
 
+local function getClientName()
+    local version = main_state.text(CLIENT_VERSION_REF) or ""
+    local lowerVersion = string.lower(version)
+
+    if string.find(lowerVersion, "endless%s*dream") then
+        return "LR2oraja ~Endless Dream~"
+    elseif string.find(lowerVersion, "lr2oraja") then
+        return "LR2oraja"
+    elseif string.find(lowerVersion, "beatoraja") then
+        return "beatoraja"
+    elseif version ~= "" then
+        return version
+    end
+
+    return "Unknown"
+end
+
 userInfo.functions.load = function ()
     local getNum = main_state.number
     local playerLabel = "Player: " .. main_state.text(2)
+    local clientLabel = getClientName()
     local dateLabel = ""
     if isViewResultDate() then
         if isViewDateOnly() then
@@ -44,8 +69,9 @@ userInfo.functions.load = function ()
 
     return {
         text = {
-            {id = "resultDate", font = 0, size = USER_INFO.SIZE * 2, constantText = playerLabel},
-            {id = "playerName", font = 0, size = USER_INFO.SIZE * 2, align = 2, constantText = dateLabel}
+            {id = "playerName", font = 0, size = USER_INFO.SIZE * 2, constantText = playerLabel},
+            {id = "clientName", font = 0, size = USER_INFO.SIZE * 2, align = 1, constantText = clientLabel},
+            {id = "resultDate", font = 0, size = USER_INFO.SIZE * 2, align = 2, constantText = dateLabel}
         }
     }
 end
@@ -55,13 +81,18 @@ userInfo.functions.dst = function ()
     local dst = skin.destination
     destinationStaticBaseWindowResult(skin, USER_INFO.WND.X, USER_INFO.WND.Y, USER_INFO.WND.W, USER_INFO.WND.H)
     dst[#dst+1] = {
+        id = "playerName", filter = 1, dst = {
+            {x = USER_INFO.PLAYER.X(USER_INFO), y = USER_INFO.PLAYER.Y(USER_INFO), w = USER_INFO.PLAYER.W, h = USER_INFO.SIZE, r = 0, g = 0, b = 0}
+        }
+    }
+    dst[#dst+1] = {
         id = "resultDate", filter = 1, dst = {
             {x = USER_INFO.DATE.X(USER_INFO), y = USER_INFO.DATE.Y(USER_INFO), w = USER_INFO.DATE.W, h = USER_INFO.SIZE, r = 0, g = 0, b = 0}
         }
     }
     dst[#dst+1] = {
-        id = "playerName", filter = 1, dst = {
-            {x = USER_INFO.NAME.X(USER_INFO), y = USER_INFO.NAME.Y(USER_INFO), w = USER_INFO.NAME.W, h = USER_INFO.SIZE, r = 0, g = 0, b = 0}
+        id = "clientName", filter = 1, dst = {
+            {x = USER_INFO.CLIENT.X(USER_INFO), y = USER_INFO.CLIENT.Y(USER_INFO), w = USER_INFO.CLIENT.W, h = USER_INFO.SIZE, r = 0, g = 0, b = 0}
         }
     }
     return skin
